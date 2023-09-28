67b2bc61ec7239148b566fbd960e9585da69c59a
Peter Bækgaard <pbakga21@student.aau.dk>
2023-09-28 11:57:26 +0200

    added Entity Class

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from components import *
 import sys
-from lib import Entity
-
+from lib.ent import Entity
 def main():
     doc = GetSpacyData.getspacydataFunc("Lars Løkke Rasmussen var statsminister i Danmark. Han er politiker for det Venstre orienterede parti.")
-
-    for ent in doc.ents:
-        print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
+    
+    ent = Entity(doc.ents[0].text, (doc.ents[0].start_char, doc.ents[0].end_char))
+    print(ent.name)
+    print(ent.index)
 if __name__ == '__main__':
     sys.exit(main())
