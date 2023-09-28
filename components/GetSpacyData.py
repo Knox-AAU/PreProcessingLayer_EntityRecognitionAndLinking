f63dfae8101b06a4f8d9bf6ee1178c36f3587afc
Peter Bækgaard <pbakga21@student.aau.dk>
2023-09-28 14:26:31 +0200

    refactored to make entity array

--- components/GetSpacyData.py
+++ components/GetSpacyData.py
@@ -1,11 +1,18 @@
 import spacy
-
+from lib.ent import Entity
 nlp = spacy.load("da_core_news_cstm")
 
 
 
-def getspacydataFunc(string):
+def GetTokens(string):
     doc = nlp(string)
     return doc
     
-    
+def GetEntities(doc):
+    ents = []
+    for entity in doc.ents: 
+        ent = Entity(entity.text, (entity.start_char, entity.end_char))
+        ents.append(ent)
+    return ents
+
+
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from components import *
 import sys
-from lib.ent import Entity
 def main():
-    doc = GetSpacyData.getspacydataFunc("Lars Løkke Rasmussen var statsminister i Danmark. Han er politiker for det Venstre orienterede parti.")
-    
-    ent = Entity(doc.ents[0].text, (doc.ents[0].start_char, doc.ents[0].end_char))
-    print(ent.name)
-    print(ent.index)
+    doc = GetSpacyData.GetTokens("Lars Løkke Rasmussen var statsminister i Danmark. Han er politiker for det Venstre orienterede parti.")
+    ents = GetSpacyData.GetEntities(doc)  
+
+    print(ents)
 if __name__ == '__main__':
     sys.exit(main())
