060f43aec090302a0880707b5162bea6845f913f
Peter Bækgaard <pbakga21@student.aau.dk>
2023-10-05 10:14:00 +0200

    added a signal.pause() to make the script run forever lol

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from components import *
 import sys
+import signal
 def main():
     doc = GetSpacyData.GetTokens("Lars Løkke Rasmussen var statsminister i Danmark. Han er politiker for det Venstre orienterede parti.")
     ents = GetSpacyData.GetEntities(doc)  
 
     print(ents)
+    signal.pause()
 if __name__ == '__main__':
     sys.exit(main())
