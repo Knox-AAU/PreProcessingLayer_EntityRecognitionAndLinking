472c91f9aec4b6b3a07628087f68169501f8ad6a
Peter Bækgaard <pbakga21@student.aau.dk>
2023-10-05 13:02:10 +0200

    changed from pause to time sleep

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from components import *
 import sys
-import signal
+import time
 def main():
     doc = GetSpacyData.GetTokens("Lars Løkke Rasmussen var statsminister i Danmark. Han er politiker for det Venstre orienterede parti.")
-    ents = GetSpacyData.GetEntities(doc)  
+    ents = GetSpacyData.GetEntities(doc)
+    let longtime = 0
 
     print(ents)
-    signal.pause()
+    while(longtime < 1000):
+        time.sleep(5)
+        longtime = longtime + 1
 if __name__ == '__main__':
     sys.exit(main())
