aa7f235308e670175e18238960a8d0189ac96c99
FredTheNoob <frederikhammer02@gmail.com>
2023-10-23 09:53:39 +0200

    add levenshtein package add example levenshtein call to EntityLinker.py

--- components/EntityLinker.py
+++ components/EntityLinker.py
@@ -1,2 +1,5 @@
-def entitylinkerFunc():
+from Levenshtein import distance
+def entitylinkerFunc(entMentions):
+    levenshteinDistance = distance("dinmor", "dinmoa")
+    print(levenshteinDistance)
     print("hello from EntityLinker.py")
