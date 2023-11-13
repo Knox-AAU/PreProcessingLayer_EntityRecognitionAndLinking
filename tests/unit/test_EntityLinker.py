bb79135964f42b979413faac6e38f1fc6bda5d92
pbaekgaard <92847415+pbaekgaard@users.noreply.github.com>
2023-11-13 10:02:53 +0100

    ready to create Gold Standard

--- tests/unit/test_EntityLinker.py
+++ tests/unit/test_EntityLinker.py
@@ -105,3 +105,49 @@ async def test_CheckIfSpaceHasBeenReplacedWithUnderscore():
 
             # Ensure the first mention links to an existing entity
             assert entLinks[0].iri == "Entity_1"
+
+
+@pytest.mark.asyncio
+async def test_entitylinkeraccuracy():
+    lastAddedID = 1
+    # Mock the database Read and Insert methods
+    mockDB = []
+
+    async def mock_read(db_path, table, predicate):
+        if table == "EntityIndex":
+            return mockDB
+        return []
+
+    async def mock_insert(db_path, table, entity_name):
+        nonlocal lastAddedID
+        if table == "EntityIndex":
+            mockDB.append((lastAddedID, entity_name))
+            lastAddedID = +1
+        return None
+
+    # Patch the Db.Read and Db.Insert functions with the mock functions
+    with patch("components.EntityLinker.Db.Read", side_effect=mock_read):
+        with patch(
+            "components.EntityLinker.Db.Insert", side_effect=mock_insert
+        ):
+            # Create some Entity instances
+            TestingDataset = {
+                "test": [
+                    Entity("Entity1", 0, 6, "file1"),
+                    Entity("newEntity3", 0, 6, "file2"),
+                ],
+                "GoldStandard": [],
+            }
+
+            # Call the entitylinkerFunc
+            entLinks = await entitylinkerFunc(
+                TestingDataset["test"], threshold=5
+            )
+            # Check the results
+            assert len(entLinks) == 2
+
+            # Ensure the first mention links to an existing entity
+            assert entLinks[0].iri == "Entity1"
+
+            # Ensure the second mention creates a new entity
+            assert entLinks[1].iri == "Entity1"
