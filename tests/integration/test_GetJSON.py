a5c29dc8ffe186e461db714a5f31098432dba93f
FredTheNoob <43958385+FredTheNoob@users.noreply.github.com>
2023-11-27 09:23:47 +0100

    fix bug where having a existing test.txt file in data_from_A would break the test_GetJSON

--- tests/integration/test_GetJSON.py
+++ tests/integration/test_GetJSON.py
@@ -12,27 +12,33 @@ directory_path = "data_from_A/"
 file_path = "data_from_A/test.txt"
 text_to_write = "Since the sudden exit of the controversial CEO Martin Kjær last week, both he and the executive board in Region North Jutland have been in hiding."
 
-# Create the directory if it doesn't exist
-os.makedirs(directory_path, exist_ok=True)
+def seedTestData():
+    # Create the directory if it doesn't exist
+    os.makedirs(directory_path, exist_ok=True)
 
-# Create the file if it doesn't exist and write text into it
-if not os.path.exists(file_path):
-    with open(file_path, 'w') as file:
-        file.write(text_to_write)
+    # Create the file if it doesn't exist and write text into it
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            file.write(text_to_write)
 
+def unseedTestData():
+    os.remove(file_path)
 
 @pytest.mark.asyncio
 async def test_SlashEntityMentionsIsUp():
+    seedTestData()
     with patch('main.DIRECTORY_TO_WATCH', directory_path):
         with TestClient(app) as client:
             res = client.get("/entitymentions?article=test.txt")
             assert res.status_code == 200
             client.__exit__
             client.close()
+            unseedTestData()
 
 
 @pytest.mark.asyncio
 async def test_SlashEntityMentionsAllReturnsJsonArray():
+    seedTestData()
     with TestClient(app) as client:
         res = client.get("/entitymentions/all")
         print(type(res.json()))
@@ -40,12 +46,15 @@ async def test_SlashEntityMentionsAllReturnsJsonArray():
         assert type(res.json()[0]) == dict
         client.__exit__
         client.close()
+        unseedTestData()
 
 @pytest.mark.asyncio
 async def test_SlashEntityMentionsReturnsJson():
+    seedTestData()
     with patch('main.DIRECTORY_TO_WATCH', directory_path):
         with TestClient(app) as client:
             res = client.get("/entitymentions?article=test.txt")
             assert type(res.json()) == dict
             client.__exit__
             client.close()
+            unseedTestData()
